d578c71a94db72f5d9c898388a060c63c8c87881
Aleksey Grin <iamdasuper@gmail.com>
2022-04-20 21:13:05 +0300

    try to commit and push after make .gitignore

---
+++ GrinAlekseyMaven/src/main/java/grin/aleksey/App.java
@@ -0,0 +1,13 @@
+package grin.aleksey;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

423a3a5b3ee4157211a372fa57c662ca7245f097
Aleksey Grin <iamdasuper@gmail.com>
2022-04-25 21:38:29 +0300

    build dependency between fists and second children

---
+++ MultiPom/firstChld/src/main/java/FistObject.java
@@ -0,0 +1,7 @@
+public class FistObject {
+    public static void main(String[] args) {
+        SecondObject secondObject = new SecondObject();
+        System.out.print("We built in first object artefact of the ");
+        System.out.println(secondObject.getName());
+    }
+}
---
+++ MultiPom/secondChld/src/main/java/SecondObject.java
@@ -0,0 +1,10 @@
+public class SecondObject {
+    private String name = "Second Object";
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return this.name;
+    }
+}
